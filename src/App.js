7be70ba85007cad636a69e1716a0864448c2c645
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-19 19:31:14 +0200

    done with props concept

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import Todo from './components/Todo';
+
 function App() {
   return (
     <div>
       <h1>My first react App.</h1>
-      <div className='card'>
-        <h2>Title</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Project with React' />
+      <Todo text='Rebuild project with React' />
     </div>
   );
 }
