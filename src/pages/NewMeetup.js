fbdc11a9e902ca0f3b43506f0b01771848cb29b1
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-20 19:45:37 +0200

    Saving data into firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,22 @@
 import NewMeetupFom from '../components/meetups/NewMeetupForm';
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-1f619-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupFom />
+      <NewMeetupFom onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
