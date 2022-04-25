794ef2de1a07072a953bab87020929f9df861bd3
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-25 08:08:05 +0200

    login and registration form

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -10,7 +10,7 @@ function MainNavigation() {
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>React With Todo App</div>
       <nav>
         <ul>
           <li>
